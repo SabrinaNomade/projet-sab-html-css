36468b6cc9bec6542f8a1a06ad35b5a5d9ad864b
sabrina <berrandousabrina@gmail.com>
2024-07-28 18:19:00 +0200

    second

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// app.js
+
 
 $(document).ready(function(){
     $('.carousel').slick({
@@ -9,5 +9,33 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const accordions = document.querySelectorAll('.accordion-header');
+
+    accordions.forEach(accordion => {
+        accordion.addEventListener('click', function() {
+            const content = this.nextElementSibling;
+            const icon = this.querySelector('.accordion-icon');
+
+            
+            content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+
+            icon.textContent = (icon.textContent === '+') ? '-' : '+';
+
+            
+            accordions.forEach(otherAccordion => {
+                if (otherAccordion !== accordion) {
+                    const otherContent = otherAccordion.nextElementSibling;
+                    const otherIcon = otherAccordion.querySelector('.accordion-icon');
+                    otherContent.style.display = 'none';
+                    otherIcon.textContent = '+';
+                }
+            });
+        });
     });
 });
